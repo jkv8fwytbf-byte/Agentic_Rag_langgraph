597275f54cd56d5ec71409aa53001d9e41ffa50b
siddharth <siddharth.muramkar1@gmail.com>
2026-07-21 14:11:52 +0530

    latest

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 def main():
     from graph.graph import app
 
+    app.get_graph().draw_mermaid_png(output_file_path="graph.png")
     result = app.invoke({"question": "What are the types of agent memory?"})
     print(result["generation"])
 

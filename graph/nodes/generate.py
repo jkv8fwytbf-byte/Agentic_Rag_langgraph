acdb31064ec33799b09ca6514ff3e7e37d485f02
siddharth <siddharth.muramkar1@gmail.com>
2026-07-21 13:35:10 +0530

    clean

--- graph/nodes/generate.py
+++ graph/nodes/generate.py
@@ -6,7 +6,5 @@ from graph.state import GraphState
 
 def generate(state: GraphState) -> Dict[str, Any]:
     print("---GENERATE---")
-    generation = generation_chain.invoke(
-        {"context": state["documents"], "question": state["question"]}
-    )
+    generation = generation_chain.invoke({"context": state["documents"], "question": state["question"]})
     return {"generation": generation}
